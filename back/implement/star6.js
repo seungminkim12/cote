7388c31c73305039d98c92ee7d1d837dbcf17d82
SeungminKim1 <ksm0057@naver.com>
2022-05-24 16:24:05 +0900

    #star5

--- back/implement/star6.js
+++ back/implement/star6.js
@@ -4,12 +4,12 @@
 
  function solution(value){     
     let star = '';
-    for(let i = 1; i < value+1; i++){        
+    for(let i = value; i > 0; i--){        
         star = '';
-        let con = 2*value-1;
-        for(let j = 0; j<con; j++){                                    
-            if(j <= Math.floor(con/2)+ con%2 - i-1 || j >= Math.floor(con/2)+ con%2 + i-1){
-                if(j >= Math.floor(con/2)+ con%2 + i){
+        let con = 2*value-1;        
+        for(let j = con; j> 0; j--){               
+            if(j <= Math.floor(con/2)+ con%2 - i || j >= Math.floor(con/2)+ con%2 + i){
+                if(j < Math.floor(con/2)+ con%2 - i){
                     continue;
                 }
                 star += ' ' 
