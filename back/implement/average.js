da83f36d9242d8a1e2b265fb681c09d34fcec8af
SeungminKim1 <ksm0057@naver.com>
2022-05-26 18:59:10 +0900

    impl #3

--- back/implement/average.js
+++ back/implement/average.js
@@ -1,15 +1,20 @@
 /**
- * 
+ * https://www.acmicpc.net/problem/1546
  */
 
-//  3
-//  40 80 60
-
-// 4
-// 10 20 0 100
-
 function solution(n,value){
-    console.log(Math.max(value.toString()))
+    value.sort((a,b) => a-b)
+    const max = value[value.length-1]
+    value = value.reduce((sum,item) => {
+        console.log('sum',sum);
+        console.log('item',item)
+        return sum + item/max*100;;
+    },0)
+    console.log('value',value)
+    console.log(value/n)
 }
 
-solution(3,[40,80,60])
+// solution(3,[40,80,60])
+// solution(4,[10,20,0,100])
+// solution(4,[1,100,100,100])
+solution(3,[10,20,30])
---
+++ back/implement/minMax.js
@@ -0,0 +1,12 @@
+/**
+ * https://www.acmicpc.net/problem/10818
+ */
+
+//  5
+//  20 10 35 30 7
+function solution(n,arr){
+    arr.sort((a,b) => a -b)
+    return `${arr[0]} ${arr[arr.length-1]}`
+}
+
+console.log(solution(n,arr))
